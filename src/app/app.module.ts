25a529da3419c59823a029b3b56d549139f92a23
sunuwars77 <=>
2020-04-16 12:11:31 +0545

    some changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import{MatTableModule} from '@angular/material/table';
+import{ MatIconModule} from '@angular/material/icon';
+import{ MatButtonModule} from '@angular/material/button';
 import { EmployeComponent } from './employe/employe.component';
 import { ShowEmpComponent } from './employe/show-emp/show-emp.component';
 import { EditEmpComponent } from './employe/edit-emp/edit-emp.component';
@@ -12,12 +15,12 @@ import { ShowDepComponent } from './department/show-dep/show-dep.component';
 import { EditDepComponent } from './department/edit-dep/edit-dep.component';
 import { AddDepComponent } from './department/add-dep/add-dep.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatRadioModule} from '@angular/material/radio';
+
 import { ContactComponent } from './contact/contact.component';
 import { EditCntComponent } from './contact/edit-cnt/edit-cnt.component';
 import { ShowCntComponent} from './contact/show-cnt/show-cnt.component';
-
+import{DepartmentService} from 'src/app/services/department.service';
+import{EmployeeService} from 'src/app/services/employee.service';
  
 
 @NgModule({
@@ -39,11 +42,11 @@ import { ShowCntComponent} from './contact/show-cnt/show-cnt.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-  
-    
     BrowserAnimationsModule,
+    MatTableModule, MatIconModule, MatButtonModule
+  
   ],
-  providers: [],
+  providers: [DepartmentService,EmployeeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/department/show-dep/show-dep.component.ts
+++ src/app/department/show-dep/show-dep.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {MatTableDataSource} from '@angular/material/table';
+import { Department } from 'src/app/models/department-model';
 @Component({
   selector: 'app-show-dep',
   templateUrl: './show-dep.component.html',
@@ -8,8 +10,25 @@ export class ShowDepComponent implements OnInit {
 
   constructor() { }
   listData : MatTableDataSource<any>
-
+displayedColumns: string[]= [ 'Options', 'DepartmentID','DepartmentName']
   ngOnInit(): void {
+    this.refreshDepList();
   }
 
+
+refreshDepList(){
+ var dummyData =[{DepartmentID:1,DepartmentName:"IT"}  , 
+     {DepartmentID:2,DepartmentName:"Finance"}] 
+
+this.listData =new MatTableDataSource(dummyData);
+
+
+}
+
+onEdit(dep:Department){
+console.log(dep)
+}
+onDelete(id:number){
+  console.log(id);
+}
 }
